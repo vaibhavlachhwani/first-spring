da276460d9f101292b3039f5688848319ff86531
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-10-27 16:05:52 +0530

    merge cofiguration class with application main class

--- src/main/java/com/vaibhav/first_spring/AppGamingSpring.java
+++ src/main/java/com/vaibhav/first_spring/AppGamingSpring.java
@@ -2,7 +2,25 @@ package com.vaibhav.first_spring;
 
 import com.vaibhav.first_spring.game.GameRunner;
 import com.vaibhav.first_spring.game.IGamingConsole;
+import com.vaibhav.first_spring.game.PacManGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AppGamingConfig {
+    @Bean
+    public IGamingConsole game() {
+        var game = new PacManGame();
+        return game;
+    }
+
+    @Bean
+    public GameRunner gameRunner(IGamingConsole game) {
+        var gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
+}
 
 public class AppGamingSpring {
     public static void main(String[] args) {
